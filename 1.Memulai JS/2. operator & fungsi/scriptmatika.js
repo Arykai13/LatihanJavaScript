cbd90a66d865fc7dd86054c5f6dc51ddf2634d95
unknown <aryakai36@gmail.com>
2024-01-15 13:34:29 +0800

    opertor dan fungsi

---
+++ 1.Memulai JS/2. operator & fungsi/scriptmatika.js
@@ -0,0 +1,38 @@
+let bil1 = 10;
+let bil2 = 3;
+
+// operator". penjumlahan
+let jumlah;
+jumlah = bil1 + bil2;
+console, log(jumlah);
+
+//operator pengurangan
+let kurang;
+kurang = bil1 - bil2;
+console.log(kurang);
+
+//operator perkalian
+let kali;
+perkalian = bil1 * bil2;
+console.log(kali);
+
+//operator pembagian
+let bagi;
+bagi = bil1 / bil2;
+console.log(bagi);
+
+//operator (pangkat)
+let pangkat;
+pangkat  = bil1 * bil2;
+console.log(pangkat);
+
+//operator (sisa hasil bagi)
+let modulus;
+modulus  = bil1 % bil2;
+console.log(modulus);
+
+//operator | Decrament
+bil1++;
+console.log(bil1);
+bil2--;
+console.log(bil2);
